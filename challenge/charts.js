c396d38b5e4318b96e03fa021b46a2b16f483c39
lydiadingess <lydiadingess@gmail.com>
2022-11-14 01:13:31 -0500

    deliverable 3

--- challenge/charts.js
+++ challenge/charts.js
@@ -121,6 +121,56 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+
+// Create a variable that holds the samples array. 
+
+    // Create a variable that filters the samples for the object with the desired sample number.
+
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadata = data.metadata;
+    var gaugeArray = metadata.filter(metaObj => metaObj.id == sample);  
+    // Create a variable that holds the first sample in the array.
+  
+
+    // 2. Create a variable that holds the first sample in the metadata array.
+      var gaugeResult = gaugeArray[0];    
+
+    // Create variables that hold the otu_ids, otu_labels, and sample_values.
+    // 3. Create a variable that holds the washing frequency.
+    var wfreqs = gaugeResult.wfreq;
+    console.log(wfreqs)  
+    // Create the yticks for the bar chart.
+   
+  
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [{
+      value: wfreqs,
+      type: "indicator",
+      mode: "gauge+number",
+      title: {text: "Belly Button Washing Frequency<br></br> Scrubs Per Week"},
+      gauge: { 
+        axis: {range: [null,10], dtick: "2"},
+        bar: {color: "black"},
+        steps:[
+          {range: [0, 2], color: "red"},
+          {range: [2, 4], color: "orange"},
+          {range: [4, 6], color: "yellow"},
+          {range: [6, 8], color: "lightgreen"},
+          {range: [8, 10], color: "green"}
+        ],
+        dtick: 2
+      }
+    }];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      automargin: true
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+
+    console.log(buildCharts)
   });
 }
 
